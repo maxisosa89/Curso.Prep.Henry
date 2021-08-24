9ae2e044fe0a6f1da1b0a768f41cf9e37edd7da7
unknown <msosa89@outlook.com>
2021-08-23 21:02:19 -0300

    JS VI

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,17 +4,31 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+  
+
+var res;
+for (let i = 0; i < nombre.length; i++){
+  if (i === 0){
+    res = (nombre[0].toUpperCase());
+  }
+  else {
+    res = res + nombre[i];
+  }
+}
+return res;
 }
 
 function invocarCallback(cb) {
   // Invoca al callback `cb`
   //Tu código:
+  cb();
 }
 
 function operacionMatematica(n1, n2, cb) {
   //Vamos a recibir una función que realiza una operación matemática como callback junto con dos números.
   //Devolver el callback pasándole como argumentos los números recibidos.
   //Tu código:
+  cb(n1, n2);
 }
 
 function sumarArray(numeros, cb) {
@@ -22,12 +36,20 @@ function sumarArray(numeros, cb) {
   // Pasa el resultado a `cb`
   // No es necesario devolver nada
   //Tu código:
+  var res = 0;
+  for (var i = 0; i < numeros.length; i++){
+    res = res + numeros[i];
+  }
+  cb(res);
 }
 
 function forEach(array, cb) {
   // Itera sobre la matriz "array" y pasa los valores al callback uno por uno
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
+  array.forEach(function(element){
+    cb(element);
+  });
 }
 
 function map(array, cb) {
@@ -35,12 +57,18 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
+  var newArr = array.map( x => cb(x));
+  return newArr;
+  
 }
 
 function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+
+  var res = array.filter(array => array.charAt(0) === 'a');
+  return res;
 }
 
 // No modificar nada debajo de esta línea
